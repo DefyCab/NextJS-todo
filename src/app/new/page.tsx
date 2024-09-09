43a65e11373dbbc87c4ee96c69f892a345e192d1
defycab <love.bellinder@gmail.com>
2024-09-09 04:38:37 +0200

    add createTodo function

--- src/app/new/page.tsx
+++ src/app/new/page.tsx
@@ -1,12 +1,21 @@
 import Link from "next/link"
 
+async function createTodo(data: FormData) {
+  "use server"
+
+  const title = data.get("title")?.valueOf()
+  if (typeof title !== "string" || title.length === 0) {
+    throw new Error("Invalid title")
+  }
+}
+
 export default function Page() {
   return (
     <>
       <header className="flex justify-between items-center mb-4">
         <h1 className="text-2xl">New</h1>
       </header>
-      <form className="flex gap-2 flex-col">
+      <form action={createTodo} className="flex gap-2 flex-col">
         <input
           type="text"
           name="title"
@@ -14,8 +23,19 @@ export default function Page() {
         border-slate-300 bg-transparent rounded px-2 py-1 outline-none
         focus-within:border-slate-100"
         />
-        <div>
-          <Link href="..">Cancel</Link>
+        <div className="flex gap-1 justify-end">
+          <Link
+            className="border border-slate-300 text-slate-300 px-2 py1 rounded hover:bg-slate-700 focus-within:bg-slate-700 outline-none"
+            href=".."
+          >
+            Cancel
+          </Link>
+          <button
+            type="submit"
+            className="border border-slate-300 text-slate-300 px-2 py1 rounded hover:bg-slate-700 focus-within:bg-slate-700 outline-none"
+          >
+            Create
+          </button>
         </div>
       </form>
     </>

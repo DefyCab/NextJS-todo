9f9b6618f701eb185af21bb52cb7d63474c78f14
defycab <love.bellinder@gmail.com>
2024-09-09 04:21:10 +0200

    adding input form to new page

--- src/app/new/page.tsx
+++ src/app/new/page.tsx
@@ -1,10 +1,23 @@
+import Link from "next/link"
+
 export default function Page() {
-    return <>
-    <header className="flex justify-between items-center mb-4">
+  return (
+    <>
+      <header className="flex justify-between items-center mb-4">
         <h1 className="text-2xl">New</h1>
       </header>
-      <form>
-        
+      <form className="flex gap-2 flex-col">
+        <input
+          type="text"
+          name="title"
+          className="border
+        border-slate-300 bg-transparent rounded px-2 py-1 outline-none
+        focus-within:border-slate-100"
+        />
+        <div>
+          <Link href="..">Cancel</Link>
+        </div>
       </form>
     </>
+  )
 }
